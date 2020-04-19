5737768db9b915787ea6cb0b23de5373a44c27e8
Mariia Smirnova <mariia.smirnova@sim-lab.ru>
2020-04-19 15:26:13 +0300

    Finished request-handling/landing.

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -11,24 +11,24 @@ counter_click = Counter()
 
 
 def index(request):
-    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    from_landing = request.GET.get('from-landing')
+    if from_landing is not None:
+        counter_click[from_landing] += 1
     return render_to_response('index.html')
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
+    landing_type = request.GET.get('ab-test-arg', 'original')
+    counter_show[landing_type] += 1
+    if landing_type == 'test':
+        return render_to_response('landing_alternate.html')
     return render_to_response('landing.html')
 
 
 def stats(request):
-    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-    # Чтобы отличить с какой версии лендинга был переход
-    # проверяйте GET параметр marker который может принимать значения test и original
-    # Для вывода результат передайте в следующем формате:
+    test = counter_click['test'] / counter_show['test'] if counter_show['test'] != 0 else 0.0
+    original = counter_click['original'] / counter_show['original'] if counter_show['original'] != 0 else 0.0
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': test,
+        'original_conversion': original,
     })
