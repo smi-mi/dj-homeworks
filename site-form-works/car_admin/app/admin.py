6c850e6fcc8c20f086c057f3bf62be1b62a0f816
Mariia Smirnova <mariia.smirnova@sim-lab.ru>
2020-05-07 13:26:16 +0300

    Refactored.

--- site-form-works/car_admin/app/admin.py
+++ site-form-works/car_admin/app/admin.py
@@ -5,6 +5,8 @@ from .forms import ReviewAdminForm
 
 
 class CarAdmin(admin.ModelAdmin):
+    readonly_fields = ['review_count']
+    Car.review_count.short_description = 'Количество статей'
     search_fields = ['brand', 'model']
     list_display = ['brand', 'model', 'review_count']
     list_filter = ['brand']
