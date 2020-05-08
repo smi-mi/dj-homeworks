e1b8bdfa1c1135e9476f716e50269d965d552ccd
Mariia Smirnova <mariia.smirnova@sim-lab.ru>
2020-05-08 13:13:11 +0300

    Done.

--- site-form-works/review/app/views.py
+++ site-form-works/review/app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import redirect, render, get_object_or_404
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 
 from .models import Product, Review
 from .forms import ReviewForm
@@ -16,18 +16,37 @@ def product_list_view(request):
     return render(request, template, context)
 
 
-def product_view(request, pk):
+class ProductView(View):
     template = 'app/product_detail.html'
-    product = get_object_or_404(Product, id=pk)
 
-    form = ReviewForm
-    if request.method == 'POST':
-        # логика для добавления отзыва
-        pass
+    def get(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
+        reviews = Review.objects.filter(product__id=pk).all()
 
-    context = {
-        'form': form,
-        'product': product
-    }
+        context = {
+            'form': ReviewForm,
+            'product': product,
+            'reviews': reviews,
+            'is_review_exist': pk in request.session.get('reviewed_products', []),
+        }
 
-    return render(request, template, context)
+        return render(request, self.template, context)
+
+    def post(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
+        reviews = Review.objects.filter(product__id=pk).all()
+
+        form = ReviewForm(request.POST)
+        Review.objects.create(text=form.data['text'], product=product)
+        if request.session.get('reviewed_products'):
+            request.session['reviewed_products'] += [pk]
+        else:
+            request.session['reviewed_products'] = [pk]
+
+        context = {
+            'product': product,
+            'reviews': reviews,
+            'is_review_exist': True,
+        }
+
+        return render(request, self.template, context)
--- site-form-works/review/product_review/urls.py
+++ site-form-works/review/product_review/urls.py
@@ -18,11 +18,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from app.views import product_list_view, product_view
+from app.views import product_list_view, ProductView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product/<int:pk>/', product_view, name='product_detail'),
+    path('product/<int:pk>/', ProductView.as_view(), name='product_detail'),
     path('', product_list_view, name='main_page'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
