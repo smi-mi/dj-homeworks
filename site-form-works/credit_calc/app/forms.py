d8f5c13ee32b008fabcce617b30dc3f717d1d6ca
Mariia Smirnova <mariia.smirnova@sim-lab.ru>
2020-05-08 14:37:21 +0300

    Done.

--- site-form-works/credit_calc/app/forms.py
+++ site-form-works/credit_calc/app/forms.py
@@ -3,16 +3,26 @@ from django import forms
 
 class CalcForm(forms.Form):
     initial_fee = forms.IntegerField(label="Стоимость товара")
-    rate = forms.CharField(label="Процентная ставка")
+    rate = forms.IntegerField(label="Процентная ставка")
     months_count = forms.IntegerField(label="Срок кредита в месяцах")
 
     def clean_initial_fee(self):
-        # валидация одного поля, функция начинающаяся на `clean_` + имя поля
         initial_fee = self.cleaned_data.get('initial_fee')
         if not initial_fee or initial_fee < 0:
             raise forms.ValidationError("Стоимость товара не может быть отрицательной")
         return initial_fee
 
+    def clean_rate(self):
+        rate = self.cleaned_data.get('rate')
+        if not rate or rate < 0:
+            raise forms.ValidationError("Процентная ставка не может быть отрицательной")
+        return rate
+
+    def clean_months_count(self):
+        months_count = self.cleaned_data.get('months_count')
+        if not months_count or months_count <= 0:
+            raise forms.ValidationError("Срок кредита должен быть строго положительным")
+        return months_count
+
     def clean(self):
-        # общая функция валидации
         return self.cleaned_data
--- site-form-works/credit_calc/app/views.py
+++ site-form-works/credit_calc/app/views.py
@@ -6,9 +6,20 @@ from .forms import CalcForm
 def calc_view(request):
     template = "app/calc.html"
 
-    form = CalcForm
-    context = {
-        'form': form
-    }
+    form = CalcForm(request.GET)
+    if form.is_valid():
+        initial_fee = int(form.data['initial_fee'])
+        rate = int(form.data['rate'])
+        months_count = int(form.data['months_count'])
+
+        common_result = initial_fee + initial_fee * rate // 100
+        result = common_result // months_count
+
+        context = {'form': form,
+                   'common_result': common_result,
+                   'result': result,
+                   }
+    else:
+        context = {'form': form}
 
     return render(request, template, context)
