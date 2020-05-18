3fd9f202e73f29e17e1e397ee5444ac8fb3e403e
Mariia Smirnova <mariia.smirnova@sim-lab.ru>
2020-05-18 16:22:08 +0300

    Done!

--- creating-project/application/table/urls.py
+++ creating-project/application/table/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import path
+from table.views import table_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('table/', table_view),
+    path('', lambda x: HttpResponseRedirect('/table/')),
 ]
-
-from table.urls import urlpatterns as table_urls
-urlpatterns = urlpatterns + table_urls
